e9a3b5ada2b6fbc75ccfb4cc4eb8635f6c197569
Sam Haider <samhaider@hotmail.com>
2025-02-27 13:06:31 -0600

    Creating admin area for the application.

---
+++ SearchAiDirectory.Function/Functions/CommentingOnTools.cs
@@ -0,0 +1,10 @@
+namespace SearchAiDirectory.Function.Functions;
+
+public class CommentingOnTools
+{
+    [Function("CommentingOnTools")]
+    public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer)
+    {
+        
+    }
+}
